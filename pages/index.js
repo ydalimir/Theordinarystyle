6fa4f6118a73c99ddbd20f4269ed5e8f3a32d913
ydalimir <ydalimir12arevalo16@outlook.es>
2021-08-04 12:19:35 -0500

    cambios
    cambios

---
+++ pages/index.js
@@ -0,0 +1,92 @@
+import BasicLayout from "../layouts/BasicLayout";
+
+
+export default function Home() {
+  return (
+    <div className="home">
+       <BasicLayout>
+       <h1>Estamos en la Home</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos</h1>
+       <h1>Productos final</h1>
+      </BasicLayout>
+     
+  
+
+    </div>
+  )
+}
